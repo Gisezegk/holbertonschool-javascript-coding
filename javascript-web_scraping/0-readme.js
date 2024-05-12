c7e3ae2fad8f2204d7f05e1d0035136620024b8e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-05-12 23:57:51 +0000

    0-reADME

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,17 @@
 
 const fs = require('fs');
 
+if (process.argv.length < 3) {
+  console.log('Usage: node read_file.js <file_path>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) console.log(error);
-  else console.log(data);
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
