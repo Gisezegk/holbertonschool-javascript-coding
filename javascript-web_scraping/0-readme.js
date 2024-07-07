9285afef1731c90ad08caf0f6cc6b7c9dc7191e7
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-07-07 22:25:56 +0000

    idk

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error('Please provide a file path as the first argument');
+  console.error(err);
   process.exit(1);
 }
 fs.readFile(filePath, 'utf8', (err, data) => {
