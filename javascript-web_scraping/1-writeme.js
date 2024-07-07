8916bc27f665c9162e64b4b408970a3c71c85b5b
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-07-07 22:47:15 +0000

    xd

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,12 +4,8 @@ const fs = require('fs');
 const filepath = process.argv[2];
 const writedata = process.argv[3];
 
-function filewriter (filepath, writedata) {
-  fs.writeFile(filepath, writedata, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-}
-
-filewriter(filepath, writedata);
+fs.writeFile(filepath, writedata, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
