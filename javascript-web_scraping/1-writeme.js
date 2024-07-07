45b3eb1ea08e50f528a8700f94a176ad2c905245
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-07-07 22:29:24 +0000

    makeup

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringy = process.argv[3];
 
-fs.writeFile(filePath, stringy, (error) => {
-  if (error) {
-    console.error(error);
+fs.writeFile(filePath, 'utf8', stringy, (err) => {
+  if (err) {
+    console.error(err);
   }
 });
