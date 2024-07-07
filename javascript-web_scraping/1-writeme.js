b08277349dbc0ea37ffd131f2de04dbe1490ad2f
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-07-07 22:37:57 +0000

    makeup

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringy = process.argv[3];
-
-if (!filePath || !stringy) {
-  console.error(err);
-  process.exit(1);
-}
-
-fs.readFile(filePath, stringy, 'utf8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', err => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
+    console.error(err); }
 });
