e8be035382d4695e52d52ec44ce50757b4d91186
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 21:32:56 +0000

    AAAA

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,26 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error("Error: No movie ID provided.");
-  process.exit(1);
-}
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
+  } else {
+    console.log(`${JSON.parse(body).title}`);
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  console.log(film.title);
 });
