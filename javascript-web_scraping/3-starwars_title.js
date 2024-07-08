0b1b553abf8335d20a43873aeefc46327e0b3512
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 22:32:12 +0000

    AAAA

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const req = require('request');
-req('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, content) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, content) => {
   if (error) {
     console.error(error);
 
