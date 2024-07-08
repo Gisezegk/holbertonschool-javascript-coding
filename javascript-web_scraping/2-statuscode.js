6f193ed9cde83405a1b27b573cbbb6e0e2a47d7c
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 20:51:04 +0000

    AAA

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
 
-request.get(process.argv[2], (err, response) => {
-  if (err) {
-    console.log(err);
+const request = require('request');
+const url = process.argv[2];
+
+if (!url) {
+  console.error("Error: No URL provided.");
+  process.exit(1);
+}
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
     return;
   }
   console.log(`code: ${response.statusCode}`);
