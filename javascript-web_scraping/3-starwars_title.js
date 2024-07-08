bd43a47054ee6e9c259dee898ff4b9947ce6be48
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 22:27:12 +0000

    AAAA

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie where the episode number matches a given integer.
-const request = require('request');
-const id = process.argv[2];
-const API = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(API, function (error, response, body) {
-  if (error != null) {
-    console.error('error:', error);
+const request = require('request');
+const dir = 'https://swapi-api.hbtn.io/api/films/';
+const id = process.argv[2];
+request(dir + id, function (error, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(response.body).title);
   }
-  const objMovie = JSON.parse(body);
-  console.log(objMovie.title);
+
 });
