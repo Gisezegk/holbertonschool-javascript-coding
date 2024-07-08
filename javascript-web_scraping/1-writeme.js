dd1e606b19b027496f761802dae446d2845e426a
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 20:43:09 +0000

    AAA

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,13 +5,15 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
+if (!filePath || !stringToWrite) {
+  console.error("Error: Missing file path or string to write.");
+  process.exit(1);
+}
 
-
-fs.writeFile(filePath, stringToWrite, 'utf8', (err, data) => {
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    console.log('File has been saved.');
   }
-  console.log(data);
-
 });
